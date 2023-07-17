6fbba3746c434ae5a844b145dbede9bf5fa269a6
haya-alhosain <hayaalhosain8@gmail.com>
2023-07-17 13:49:36 +0300

    way two to fetch data

--- script.js
+++ script.js
@@ -1,56 +1,96 @@
-//GET Users
-function getUsers() {
-  let request = new XMLHttpRequest();
-  request.open("GET", "https://jsonplaceholder.typicode.com/users");
-  request.responseType = "json";
-  request.send();
-  request.onload = function () {
-    if (request.status >= 200 && request.status < 300) {
-      let users = request.response;
-      users.map((user) => {
-        document.querySelector(".names").innerHTML += `
-        <div class="user" onclick="userClicked(${user.id})">
-        <h4 class="userName">${user.name}</h4>
-        <span class="userEmail">${user.email}</span>
-        </div>`;
-      });
-    } else {
-      alert("error");
-    }
-  };
+//GET Users Way 1
+async function getUsers() {
+  await fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.json())
+    .then((res) => UsersData(res));
 }
-
-//GET Posts
-function getPosts(usId) {
-  let request = new XMLHttpRequest();
-  request.open(
-    "GET",
-    "https://jsonplaceholder.typicode.com/posts?userId=" + usId
-  );
-  request.responseType = "json";
-  request.send();
-  request.onload = function () {
-    if (request.status >= 200 && request.status < 300) {
-      let posts = request.response;
-      document.querySelector(".start").innerHTML = "";
-      document.querySelector(".content").innerHTML = "";
-      posts.map((post) => {
-        document.querySelector(".content").innerHTML += `
-        <div class="post">
-        <h3 class="TitlePost">${post.title}</h3>
-        <p class="bodyPost">${post.body}</p>
-        </div>`;
-      });
-    } else {
-      alert("error");
-    }
-  };
+function UsersData(res) {
+  res.map((user) => {
+    document.querySelector(".names").innerHTML += `
+          <div class="user" onclick="userClicked(${user.id})">
+          <h4 class="userName">${user.name}</h4>
+          <span class="userEmail">${user.email}</span>
+          </div>`;
+  });
 }
-
-//Filteration
-
 getUsers();
 
+//GET Posts Way 1
+async function getPosts(usId) {
+  await fetch("https://jsonplaceholder.typicode.com/posts?userId=" + usId)
+    .then((res) => res.json())
+    .then((res) => PostsData(res));
+}
+function PostsData(res) {
+  document.querySelector(".start").innerHTML = "";
+  document.querySelector(".content").innerHTML = "";
+  res.map((post) => {
+    document.querySelector(".content").innerHTML += `
+          <div class="post">
+          <h3 class="TitlePost">${post.title}</h3>
+          <p class="bodyPost">${post.body}</p>
+          </div>`;
+  });
+}
 function userClicked(id) {
   getPosts(id);
 }
+
+// ============================================================================================
+
+//GET Users Way 2
+// function getUsers() {
+//   let request = new XMLHttpRequest();
+//   request.open("GET", "https://jsonplaceholder.typicode.com/users");
+//   request.responseType = "json";
+//   request.send();
+//   request.onload = function () {
+//     if (request.status >= 200 && request.status < 300) {
+//       let users = request.response;
+//       users.map((user) => {
+//         document.querySelector(".names").innerHTML += `
+//         <div class="user" onclick="userClicked(${user.id})">
+//         <h4 class="userName">${user.name}</h4>
+//         <span class="userEmail">${user.email}</span>
+//         </div>`;
+//       });
+//     } else {
+//       alert("error");
+//     }
+//   };
+// }
+
+//GET Posts Way 2
+// function getPosts(usId) {
+//   let request = new XMLHttpRequest();
+//   request.open(
+//     "GET",
+//     "https://jsonplaceholder.typicode.com/posts?userId=" + usId
+//   );
+//   request.responseType = "json";
+//   request.send();
+//   request.onload = function () {
+//     if (request.status >= 200 && request.status < 300) {
+//       let posts = request.response;
+//       document.querySelector(".start").innerHTML = "";
+//       document.querySelector(".content").innerHTML = "";
+//       posts.map((post) => {
+//         document.querySelector(".content").innerHTML += `
+//         <div class="post">
+//         <h3 class="TitlePost">${post.title}</h3>
+//         <p class="bodyPost">${post.body}</p>
+//         </div>`;
+//       });
+//     } else {
+//       alert("error");
+//     }
+//   };
+// }
+
+// //Filteration
+
+// getUsers();
+
+// function userClicked(id) {
+//   getPosts(id);
+// }
